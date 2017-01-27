faac5c0dd2e8acf63bee1f797a33ee443e403afd
gnieboer <gnieboer@corpcomm.net>
2017-01-27 09:23:49 -0500

    Linked scipy to static lib vice dynamic

--- Step4-BuildPythonPackages.ps1
+++ Step4-BuildPythonPackages.ps1
@@ -444,7 +444,7 @@ Function SetupPython
 		& $pythonroot/$pythonexe setup.py clean  2>&1 >> $log
 		& $pythonroot/$pythonexe setup.py config --fcompiler=intelvem --compiler=msvc  2>&1 >> $log
 		Write-Host -NoNewline "building..."
-		$env:_LINK_=" /NODEFAULTLIB:""LIBCMT.lib"" /NODEFAULTLIB:""LIBMMT.lib"" /DEFAULTLIB:""LIBMMD.lib"" "
+		$env:_LINK_=" /NODEFAULTLIB:""LIBCMT.lib"" /NODEFAULTLIB:""LIBMMT.lib"" /DEFAULTLIB:""LIBMMDS.lib"" "
 		# setup.py doesn't handle debug flag correctly for windows ifort, it adds a -g flag which is ambiguous so we'll do our best to emulate it manually
 		if ($configuration -match "Debug") {$env:__INTEL_POST_FFLAGS = " /debug:all "} else {$env:__INTEL_POST_FFLAGS = ""}
 		& $pythonroot/$pythonexe setup.py build --compiler=msvc --fcompiler=intelvem 2>&1 >> $log
