07515d3b680bdc454c9edfc04c4e7d00f0583f2e
gnieboer <gnieboer@corpcomm.net>
2017-06-26 16:11:46 -0400

    stage4: consolidate pdb files

--- Step9-BuildMSI.ps1
+++ Step9-BuildMSI.ps1
@@ -45,9 +45,22 @@ Function BuildMSI {
 	}
 }
 
-if ($configmode -eq "1" -or $configmode -eq "all") {BuildMSI "Release"}
+Function ConsolidatePDBs {
+	$configuration = $args[0]
+	New-Item -ItemType Directory -Force $root\src-stage4-installer\symbols\$configuration 2>&1 >> $Log
+	pushd $root\src-stage1-dependencies
+	Get-ChildItem -Recurse -Filter "$configuration" -Directory | Get-ChildItem -Recurse -Directory | Get-ChildItem -Filter "*.pdb" | Copy-Item -Destination ..\src-stage4-installer\symbols\$configuration -Force
+	Get-ChildItem -Recurse -Filter "${configuration}DLL" -Directory | Get-ChildItem -Recurse -Directory | Get-ChildItem -Filter "*.pdb" | Copy-Item -Destination ..\src-stage4-installer\symbols\$configuration -Force
+	popd
+	pushd $root\src-stage3 
+	Get-ChildItem -Recurse -Filter "$configuration" -Directory | Get-ChildItem -Recurse -Directory | Get-ChildItem -Filter "*.pdb" | Copy-Item -Destination ..\src-stage4-installer\symbols\$configuration -Force
+	Get-ChildItem -Recurse -Filter "${configuration}DLL" -Directory | Get-ChildItem -Recurse -Directory | Get-ChildItem -Filter "*.pdb" | Copy-Item -Destination ..\src-stage4-installer\symbols\$configuration -Force
+	popd
+}
+
+if ($configmode -eq "1" -or $configmode -eq "all") {BuildMSI "Release"; ConsolidatePDBs "Release"}
 if ($configmode -eq "2" -or $configmode -eq "all") {BuildMSI "Release-AVX2"}
-if ($configmode -eq "3" -or $configmode -eq "all") {BuildMSI "Debug"}
+if ($configmode -eq "3" -or $configmode -eq "all") {BuildMSI "Debug"; ConsolidatePDBs "Debug"}
 
 ""
 "COMPLETED STEP 9: .msi files have been created and can be found in $root/src-stage4-installer/dist/(configuration)/gnuradio_$gnuradio_version_win64.msi"
