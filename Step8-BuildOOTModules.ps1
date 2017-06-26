57c36dab37f6132aa45c0f0d1c71c57683943d3e
gnieboer <gnieboer@corpcomm.net>
2017-06-26 09:05:04 -0400

    debug: renamed _swig files so debug will work.

--- Step8-BuildOOTModules.ps1
+++ Step8-BuildOOTModules.ps1
@@ -1496,6 +1496,19 @@ function BuildOOTModules
 		$env:_CL_ = ""
 	    "complete"
     }
+
+	#the swig libraries aren't properly named for the debug build, so do it here
+	if ($configuration -match "Debug") {
+		pushd $root/src-stage3/staged_install/$configuration
+		Get-ChildItem -Filter "*_swig.pyd" -Recurse | Move-Item -Force -Destination {$_.FullName -replace "_swig","_swig_d" } 
+		Get-ChildItem -Filter "*_swig0.pyd" -Recurse | Move-Item -Force -Destination {$_.FullName -replace "_swig0","_swig0_d" } 
+		Get-ChildItem -Filter "*_swig1.pyd" -Recurse | Move-Item -Force -Destination {$_.FullName -replace "_swig1","_swig1_d" } 
+		Get-ChildItem -Filter "*_swig2.pyd" -Recurse | Move-Item -Force -Destination {$_.FullName -replace "_swig2","_swig2_d" } 
+		Get-ChildItem -Filter "*_swig3.pyd" -Recurse | Move-Item -Force -Destination {$_.FullName -replace "_swig3","_swig3_d" } 
+		Get-ChildItem -Filter "*_swig4.pyd" -Recurse | Move-Item -Force -Destination {$_.FullName -replace "_swig4","_swig4_d" } 
+		Get-ChildItem -Filter "*_swig5.pyd" -Recurse | Move-Item -Force -Destination {$_.FullName -replace "_swig5","_swig5_d" } 
+		popd
+	}
 }
 
 # Release build
