9869570cd193bb83083df4d9e86cb0768d01d70a
Travis Collins <travis.collins@analog.com>
2018-08-29 17:31:25 -0700

    Add initial appveyor pieces

---
+++ RUNME_AUTO.ps1
@@ -0,0 +1,66 @@
+# GNURadio Windows Build System
+# Geof Nieboer
+#
+# RUNME_FIRST.ps1
+#
+$ErrorActionPreference = "Stop"
+
+#$Global:root = Read-Host "Please choose an absolute root directory for this build. <c:\gr-build>"
+$Global:root = "C:\grb"
+if (!$root) {$root = "C:\gr-build"}
+if (!(Test-Path -isValid -LiteralPath $root)) {
+    Write-Host "'$root' is not a valid path.  Exiting script."
+    return
+}
+if (![System.IO.Path]::IsPathRooted($root)) {
+    Write-Host "'$root' is not an absolute path.  Exiting script."
+    return
+}
+if ($root.length -gt 11)
+{
+    Write-Host "'$root' cannot be longer than 11 chars due to filename length errors that would occur during build, please choose a different root.  Exiting script."
+	# These errors will manifest themselves in a couple places, but tensorflow is currently the most sensitive to this problem.
+    return
+}
+
+# setup
+"Performing initial setup"
+Write-Host "Setting up directories and checking dependencies..."
+
+# set up enough to import setup.ps1 and start logging
+New-Item -ItemType Directory -Force -Path $root > $null
+New-Item -ItemType Directory -Force -Path "$root\logs" > $null
+
+if ($script:MyInvocation.MyCommand.Path -eq $null) {
+    $mypath = "."
+} else {
+    $mypath =  Split-Path $script:MyInvocation.MyCommand.Path
+}
+. $mypath\Setup.ps1 -Force
+ResetLog
+SetLog "Initial Configuration"
+
+# build basic directories
+
+New-Item -ItemType Directory -Force -Path "$root\bin" >> $Log
+New-Item -ItemType Directory -Force -Path "$root\build" >> $Log
+New-Item -ItemType Directory -Force -Path "$root\include" >> $Log
+New-Item -ItemType Directory -Force -Path "$root\packages" >> $Log
+New-Item -ItemType Directory -Force -Path "$root\src-stage1-dependencies" >> $Log
+New-Item -ItemType Directory -Force -Path "$root\src-stage2-python" >> $Log
+New-Item -ItemType Directory -Force -Path "$root\src-stage3" >> $Log
+New-Item -ItemType Directory -Force -Path "$root\src-stage3\src" >> $Log
+New-Item -ItemType Directory -Force -Path "$root\src-stage3\oot_code" >> $Log
+New-Item -ItemType Directory -Force -Path "$root\src-stage4-installer" >> $Log
+New-Item -ItemType Directory -Force -Path "$root\scripts" >> $Log
+robocopy $mypath/bin $root/bin /e >> $Log
+Copy-Item $mypath/wix/*.* $root/src-stage4-installer -Recurse -Force >> $Log
+Copy-Item $mypath/run/*.bat $root\src-stage3\src -Force
+Copy-Item $mypath/*.ps1 $root/scripts -Force >> $Log
+Copy-Item $mypath/*.psd1 $root/scripts -Force >> $Log
+Copy-Item $mypath/icons $root\src-stage3 -Recurse -Force >> $Log
+Copy-Item $mypath/AVX2.props $root/src-stage1-dependencies -Force >> $Log
+Remove-Item $root/scripts/~RUNME_FIRST.ps1 >> $Log  # Don't need this file in the build tree after everything is there
+cd $root/scripts
+
+& .\Step1-UserPreferences_AUTO.ps1
---
+++ Step1-UserPreferences_AUTO.ps1
@@ -0,0 +1,134 @@
+# GNURadio Windows Build System
+# Geof Nieboer
+#
+Write-Host "Welcome to GNURadio windows build and installation script."
+Write-Host ""
+Write-Host "This script can build GNURadio and every dependency needed using Visual Studio 2015"
+Write-Host "to ensure version and binary compatibility."
+Write-Host ""
+Write-Host "This build script also includes a number of OOT packages in the final installer"
+Write-Host "This version currently builds and includes:"
+Write-Host "- UHD drivers"
+Write-Host "- airspy drivers"
+Write-Host "- bladeRF drivers"
+Write-Host "- RTL-SDR drivers"
+Write-Host "- HackRF drivers"
+Write-Host "- gr-osmocomsdr"
+Write-Host "- gr-iqimbalance"
+Write-Host "- gr-benchmark"
+Write-Host "- gr-fosphor"
+Write-Host "- gr-adsb"
+Write-Host "- gr-acars2"
+Write-Host "- gr-iio"
+Write-Host ""
+Write-Host "The only GNURadio component not currently built is gr-comedi."
+Write-Host ""
+Write-Host "You must be connected to the internet to run this script."
+Write-Host "Downloaded packages will be cached in the packages subdir."
+Write-Host ""
+Write-Host "This script can build GNURadio in two different ways:"
+Write-Host "1- Compile every dependency from source right now [LONGEST]"
+Write-Host "2- Download all dependencies, including custom python, as binaries and build GNURadio"
+Write-Host "   only from source"
+Write-Host ""
+
+$buildoption = "2"
+
+<#
+$buildoption = Read-Host "Please choose an option (1,2)<2>"
+if (!$buildoption) {$buildoption = "2"}
+if ($buildoption -ne "1" -and $buildoption -ne "2" )
+{
+    Write-Host "'$buildoption' was not a valid choice.  Exiting script."
+    return
+}
+Write-Host ""
+#>
+
+if ($script:MyInvocation.MyCommand.Path -eq $null) {
+    $mypath = "."
+} else {
+    $mypath =  Split-Path $script:MyInvocation.MyCommand.Path
+}
+. $mypath\Setup.ps1 -Force
+SetLog "Initial Configuration"
+
+Write-Host "Dependencies checked passed: VS 2015, git, perl, cmake, 7-zip, & doxygen are installed."
+if ($hasIFORT -and $buildoption -eq "1")
+{
+    Write-Host "Intel Fortran compiler has been detected.  Numpy/Scipy will be built from source"
+} elseif ($buildoption -eq "1") {
+    Write-Host "Intel Fortran compiler not installed.  Numpy/Scipy will be installed from wheels"
+}
+Write-Host ""
+
+$defaultroot = Split-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path)
+#$Global:root = Read-Host "Please choose an absolute root directory for this build <$defaultroot>"
+if (!$root) {$root = $defaultroot}
+if (!(Test-Path -isValid -LiteralPath $root)) {
+    Write-Host "'$root' is not a valid path.  Exiting script."
+    return
+}
+if (![System.IO.Path]::IsPathRooted($root)) {
+    Write-Host "'$root' is not an absolute path.  Exiting script."
+    return
+}
+# need this fixed for the qt.conf file in Step5/5a
+$root = $root -replace "\\", "/"
+
+if ( $buildoption -eq "1")
+{
+	Write-Host ""
+	Write-Host "By default, GNURadio uses only open source libraries.  However, if you have Intel MKL installed"
+	Write-Host "the script can find it and use it to build numpy and scipy, which are significantly faster than"
+	Write-Host "the MSVC version of OpenBLAS.  However, the resulting products CANNOT be distributed as it"
+	Write-Host "would violate the GPL."
+	Write-Host ""
+	$useMKLstr = Read-Host "Use MKL libraries if available? <N>"
+}
+if ($useMKLstr -match "y") {$Global:BuildNumpyWithMKL = $true} else {$Global:BuildNumpyWithMKL = $false}
+
+if ( $buildoption -eq "2")
+{
+	Write-Host ""
+	Write-Host "This script can build 3 different configurations of GNURadio:"
+	Write-Host "1- Release"
+	Write-Host "2- Release, optimized for AVX2 cpu's only.  Fastest, but will crash on non-AVX2 processors!"
+	Write-Host "3- Debug"
+	Write-Host "Or, the script can build all three versions so all the dependencies are available to you."
+	Write-Host ""
+  $Global:configmode = "1"
+	#$Global:configmode = Read-Host "What configuration do you want to build? (1,2,3,all)<1>"
+}
+if (!$configmode) {$configmode = "all"}
+if (($configmode -ne "1" -and $configmode -ne "2" -and $configmode -ne "3" -and $configmode -ne "all")) {
+    Write-Host "That was not a valid choice.  Exiting script"
+    return
+}
+
+if ($buildoption -eq "1") {$numberof = 2}
+if ($buildoption -eq "2") {$numberof = .3}
+if ($configmode -eq "all") {$numberof = $numberof * 3}
+
+Write-Host ""
+Write-Host "Thank you.  Package download will occur first, then compilation and build will begin."
+Write-Host "Building will take about $numberof hours on a Intel i7-5930X machine"
+Write-Host "After downloads are complete.  Logs can be found in $root/Logs if the build fails."
+Write-Host ""
+
+# RUN
+
+if ($buildoption -eq "1")
+{
+	& $root\scripts\Step2-GetStage1Packages.ps1
+	& $root\scripts\Step3-BuildStage1Packages.ps1
+	& $root\scripts\Step4-BuildPythonPackages.ps1
+	& $root\scripts\Step5-ConsolidateLibs.ps1
+} else {
+    & $root\scripts\Step5a-DownloadDependencies.ps1
+}
+
+& $root\scripts\Step6-GetStage3Packages.ps1
+& $root\scripts\Step7-BuildGNURadio.ps1 $configmode
+& $root\scripts\Step8-BuildOOTModules.ps1 $configmode
+& $root\scripts\Step9-BuildMSI.ps1 $configmode
